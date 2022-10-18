50a28d4e7827bfc6903ed78de4751c460b06e715
Carlos Antonino <carlosantonino.cs@gmail.com>
2022-10-18 14:51:25 -0300

    Colocando imagens na pag principal e logo

--- src/components/header.js
+++ src/components/header.js
@@ -3,6 +3,7 @@ export default function Header() {
     return(
         <>
             <header>
+                {/* <img src="img/logo.jpg" /> */}
                 <h1>Logo</h1>
                 <ul className="menu">
                     <li>Inicio</li>
