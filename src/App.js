a0f5e9c5b54df39691b5abd0d1a81fc59c3a1662
Carlos Antonino <carlosantonino.cs@gmail.com>
2022-10-18 12:25:10 -0300

    Criando o Main

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import Header from './components/header';
+import Footer from './components/footer';
+import Main from './components/main';
 
 function App() {
   return (
     <>
       <Header />
+      <Main />
+      <Footer />
     </>
   );
 }
---
+++ src/components/main.js
@@ -0,0 +1,11 @@
+
+export default function Main() {
+    return (
+        <>
+            <section>
+                <h1>Inicio</h1>
+                
+            </section>
+        </>
+    )
+}
