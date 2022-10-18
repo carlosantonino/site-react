f5284f7667c961ca6b172439ec87dc5baece5168
Carlos Antonino <carlosantonino.cs@gmail.com>
2022-10-18 10:48:01 -0300

    criando o cabeçalho do site

---
+++ src/components/header.js
@@ -0,0 +1,12 @@
+
+export default function Header() {
+    return(
+        <>
+            <ul>
+                <li>Inicio</li>
+                <li>Sobre</li>
+                <li>Contato</li>
+            </ul>  
+        </>
+    )
+}
