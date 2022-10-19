379692cf06a19cb77f08975a4369baa3ee2c641e
Carlos Antonino <carlosantonino.cs@gmail.com>
2022-10-19 09:43:32 -0300

    tatu

--- src/components/main.js
+++ src/components/main.js
@@ -3,42 +3,54 @@ export default function Main() {
     return (
         <>
             <div className="main">
-            <h1 className="titulo">Inicio</h1> 
-            <section>                
-                <div>
-                    <img src="/img/cachorrinho.jpg" />
-                    <h2>Cachorrinho</h2>
-                    </div>             
+                <h1 className="titulo">Inicio</h1> 
+                <section>                
                     <div>
-                    <img src="/img/elefante.jpg" />
-                    <h2>Elefante</h2>                
+                        <img src="/img/cachorrinho.jpg" />
+                        <h2>Cachorrinho</h2>
                     </div>
+
                     <div>
-                    <img src="/img/golfinho.jpg" />
-                    <h2>Golfinho</h2>                
+                        <img src="/img/elefante.jpg" />
+                        <h2>Elefante</h2>                
                     </div>
+
                     <div>
-                    <img src="/img/manada.jpg" />
-                    <h2>Animais selvagens</h2>
+                        <img src="/img/golfinho.jpg" />
+                        <h2>Golfinho</h2>                
                     </div>
+                    
                     <div>
-                    <img src="/img/onca.jpg" />
-                    <h2>Onça</h2>
-                    </div>                
-                    <div>
-                    <img src="/img/papagaio.jpg" />
-                    <h2>Papagaio</h2>
+                        <img src="/img/manada.jpg" />
+                        <h2>Animais selvagens</h2>
                     </div>
+
                     <div>
-                    <img src="/img/coelho.jpeg" />
-                    <h2>Coelho</h2>
+                        <img src="/img/onca.jpg" />
+                        <h2>Onça</h2>
                     </div>
+
                     <div>
-                    <img src="/img/gato.jpeg" />
-                    <h2>Gato</h2>
-                </div>
-                                
-            </section>
+                        <img src="/img/papagaio.jpg" />
+                        <h2>Papagaio</h2>
+                    </div>
+
+                    <div>
+                        <img src="/img/coelho.jpeg" />
+                        <h2>Coelho</h2>
+                    </div>
+
+                    <div>
+                        <img src="/img/gato.jpeg" />
+                        <h2>Gato</h2>
+                    </div>
+
+                    <div>
+                        <img src="/img/tatu.jpg" />
+                        <h2>Tatu</h2>
+                    </div>
+
+                </section>
             </div>
         </>
     )
