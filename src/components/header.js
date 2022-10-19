3fd52b8074b9521216cddeb856e6d8ed08889e7a
Carlos Antonino <carlosantonino.cs@gmail.com>
2022-10-19 12:47:42 -0300

    adequando cabeçalho para menu resp

--- src/components/header.js
+++ src/components/header.js
@@ -1,13 +1,17 @@
-
-export default function Header() {
+export default function Header() {    
+    // const btnMobile = document.getElementById('btn-mobile');
+    // function toggleMenu(){
+    // const nav = document.getElementById('nav');
+    // nav.classList.toggle('active');
+    // }
+    // btnMobile.addEventListener('click', toggleMenu);
     return(
         <>
             <header id="header">
-                <img src="img/logo_mundo_animal.png" />
-                {/* <h1>Logo</h1> */}
-                <nav>   
+                <img src="img/logo_mundo_animal.png" alt="teste" />
+                <nav id="nav">   
                     <button id="btn-mobile">Menu</button>
-                    <ul className="menu">
+                    <ul id="menu">
                         <li><a href="/">Inicio</a></li>
                         <li><a href="/">Portfólio</a></li>
                         <li><a href="/">Sobre</a></li>
